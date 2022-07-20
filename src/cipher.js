8d39571591e28cfc2d8db1e5be53db6c64ff74cf
Amanda <amandaoliveirapolari@gmail.com>
2022-07-19 21:17:50 -0300

    feat: primeiro commitinho do projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,14 @@
 const cipher = {
-  // ...
+  encode: function(offset, mensagem) {
+    // algoritmo da cifra de cesar para codificar mensagem
+    const msgCifrada = mensagem // por enquanto retorna msg original
+    return msgCifrada
+  },
+  decode: function(offset, mensagem) {
+    // algoritmo da cifra de cesar para decodificar mensagem
+    const msgDecifrada = mensagem // por enquanto retorna msg original
+    return msgDecifrada
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+const offsetValor = document.getElementById("offset").value;
+const mensagemOriginal = document.getElementById("mensagem-original");
+const mensagemCifrada = document.getElementById("mensagem-cifrada");
+
+function cifraDeCesar(event) {
+  const oQueFoiDigitado = event.srcElement.value;
+  const idDeOndeOcorreuOevento = event.srcElement.id;
+
+  if (idDeOndeOcorreuOevento == mensagemOriginal.id) {
+    mensagemCifrada.value = cipher.encode(offsetValor, oQueFoiDigitado); // chama funcao que cifra
+  } else {
+    mensagemOriginal.value = cipher.decode(offsetValor, oQueFoiDigitado); // chama funcao que decifra
+  }
+}
+
+mensagemOriginal.addEventListener("input", cifraDeCesar);
+mensagemCifrada.addEventListener("input", cifraDeCesar);

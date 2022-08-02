b9ec9657025fe880fe3d24dc66b68837c43bf3c2
Amanda <amandaoliveirapolari@gmail.com>
2022-08-02 13:18:35 -0300

    remoção de comentários

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,23 @@
 const ALFABETO = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 //-------> Codificar letra por letra
-
-// Uma verificação de minúscula para tratar do jeito correspondente
 function verificaMinuscula(letra) {
   return /[a-z]/.test(letra);
 }
-// Lembrando: verificaMinuscula(letra)
-// retorna true ou false
 
-// Esta é uma função que cifra uma letra, e para isso 
-// essa função recebe o offset e a letra em questão
 function cifraLetra(offset, letra) {
-  // Os comentários a partir daqui são do algoritmo escrito:
-  // 1.Obter o número da posição inicial da letra no alfabeto
   const posicaoInicialDaLetra = ALFABETO.indexOf(letra);
-  // 1.1 Verificar se a letra não pertence ao alfabeto,
-  // caso não verdadeiro, retorne a própria letra
   if (posicaoInicialDaLetra < 0 || letra == "") {
     return letra;
   }
-  // 2.Somar o offset ao número inicial da letra
   const resultadoSoma = offset + posicaoInicialDaLetra;
-  // 3. Se o resultado da soma for:
-  // positivo: aplicar % 26 (para corrigir o excesso)
-  // negativo: aplicar % 26 (para corrigir o excesso) + 26 (tamanho do alfabeto)
   let novaPosicaoDaLetra;
   if (resultadoSoma >= 0) {
     novaPosicaoDaLetra = resultadoSoma % 26;
   } else {
     novaPosicaoDaLetra = (resultadoSoma % 26) + 26;
   }
-  // 4. Se a letra que entrou for minúscula,
-  // retornar a letra da posição correspondente.
-  // senão, retornar a letra da posição correspondente
-  // acrescida de 26.
+
   if (verificaMinuscula(letra)) {
     return ALFABETO.charAt(novaPosicaoDaLetra);
   }
@@ -42,33 +25,21 @@ function cifraLetra(offset, letra) {
 }
 
 // ------> Codificar mensagem inteira
-
 function codificaMensagem(offset, mensagemCompleta) {
-  // 1. Defini a variável vazia para
-  // representar a mensagem codificada
   let mensagemCodificada = "";
-  // 2. Percorro a mensagem letra por letra,
-  // codifico cada letra e salvo essa codificação
-  // na variável do passo 1
   let letra;
   for (let i = 0; i < mensagemCompleta.length; i = i + 1) {
     letra = mensagemCompleta.charAt(i);
     mensagemCodificada = mensagemCodificada + cifraLetra(offset, letra);
   }
-  // 3. Retornar a variável do passo 1
   return mensagemCodificada;
 }
 
-// cipher é um objeto javascript como tese pediu
-// dentro dele eu tenho as funções encode e decode
 const cipher = {
   encode: function (offset, mensagem) {
     if(typeof offset != 'number' || typeof mensagem != 'string') {
       throw TypeError();
     }
-    // Condicional para que se o offset for diferente de
-    // um número, ou a mensagem for diferente de uma string
-    // dispara um tyerror como o teste pediu
 
     return codificaMensagem(offset % 26, mensagem);
   },
@@ -76,9 +47,6 @@ const cipher = {
     if(typeof offset != 'number' || typeof mensagem != 'string') {
       throw TypeError();
     }
-    // Condicional para que se o offset for diferente de
-    // um número, ou a mensagem for diferente de uma string
-    // dispara um tyerror como o teste pediu
 
     return codificaMensagem(-(offset % 26), mensagem);
   },
--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
 import cipher from "./cipher.js";
 
-// Captura dos elementos html que irei utilizar:
 const offset = document.getElementById("offset");
 const mensagemOriginal = document.getElementById("mensagem-original");
 const mensagemCifrada = document.getElementById("mensagem-cifrada");
 
-// Descrição da função anterior ao seu chamamento para que ela já esteja 
-// carregada quando for chamada posteriormente
 function cifraDeCesar(event) {
 
-  // Para facilitar minha leitura e entendimento futuro, 
-  // salvei em uma variável o caminho do evento
   const oQueFoiDigitado = event.srcElement.value;
   const idDeOndeOcorreuOevento = event.srcElement.id;
   const offsetValor = Number(offset.value);
 
-  // Se o id de onde ocorreu o evento for igual 
-  // ao id da caixa da mensagem digitada
   if (idDeOndeOcorreuOevento == mensagemOriginal.id) {
-
-    // Chamo a função cipher.encode
-    // E coloco o seu resultado no valor da caixa mensagem cifrada
-    mensagemCifrada.value = cipher.encode(offsetValor, oQueFoiDigitado); // chama funcao que cifra
+    mensagemCifrada.value = cipher.encode(offsetValor, oQueFoiDigitado);
   } else { 
-    // Se não, 
-    // Chamo a função cipher.decode
-    // E coloco o seu resultado no valor da caixa mensagem decifrada
-    mensagemOriginal.value = cipher.decode(offsetValor, oQueFoiDigitado); // chama funcao que decifra
+    mensagemOriginal.value = cipher.decode(offsetValor, oQueFoiDigitado);
   }
 }
 
-// Aplicação do addEventListener para o evento de escrita(input) nas caixas, 
-// que chama a função cifraDeCesar
 mensagemOriginal.addEventListener("input", cifraDeCesar);
 mensagemCifrada.addEventListener("input", cifraDeCesar);
